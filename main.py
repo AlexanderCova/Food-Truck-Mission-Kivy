768b99b4f3dfac1242c7a9a3a6423363d3f0d962
Alexander <covaalexander06@gmail.com>
2021-09-14 14:25:27 -0400

    added a whole bunch of crap to the app

--- main.py
+++ main.py
@@ -16,13 +16,15 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import (ScreenManager, Screen, NoTransition,
 SlideTransition, CardTransition, SwapTransition,
 FadeTransition, WipeTransition, FallOutTransition, RiseInTransition) 
+from kivy.graphics import Color, Rectangle, Canvas
 from kivy.lang import Builder
 from datetime import date, datetime, timedelta
 import mysql.connector
 from plyer import notification
 import smtplib, ssl
 import re
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 WindowSize = Window.size
 Window.clearcolor = (33/255, 33/255, 33/255, 1)
@@ -46,6 +48,10 @@ htmlMSG = """Subject: Thanks For Creating a FoodTruckFinderMI Account!
 
             We hope you enjoy our new app!!
         """
+truckMSG = """Subject: Thanks For Registering Your Truck ON FoodTruckFinderMI
+
+
+            Thanks, we hope you enjoy the new app"""
 
 # The ScreenManager controls movin0 between screens
 screen_manager = ScreenManager()
@@ -61,6 +67,10 @@ cnx = mysql.connector.connect(
 )
 
 cursor = cnx.cursor()
+
+
+color = (200, 200, 200, .5)
+
 
 class MainApp(App):
     
@@ -89,10 +99,10 @@ class LoginScreen(Screen):
         emailValue = self.ids['email'].text
         passwordValue = self.ids['password'].text
         
-        cursor.execute(f"SELECT email from users WHERE email='{email}' AND password = '{password}';")
+        cursor.execute(f"SELECT email from users WHERE email='{emailValue}' AND password = '{passwordValue}';")
 
-        cur.execute(statement)
-        if not cur.fetchone():  # An empty result evaluates to False.
+        
+        if not cursor.fetchone():  # An empty result evaluates to False.
             print("Login failed")
         else:
             screen_manager.transition.direction = 'left'
@@ -134,35 +144,170 @@ class CreateAccountScreen(Screen):
             cnx.commit()
             context = ssl.create_default_context()
 
-            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-                server.login(senderEmail, emailPassword)
-                server.sendmail(senderEmail, emailValue, htmlMSG)
-            
-            screen_manager.transition.direction = 'left'
-            screen_manager.current = 'user_screen'
+            try:
+                with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                    server.login(senderEmail, emailPassword)
+                    server.sendmail(senderEmail, emailValue, htmlMSG)
+                
+                
+            except:
+                screen_manager.transition.direction = 'left'
+                screen_manager.current = 'user_screen'
+                
         else:
             label = Label(text='Invalid Email Address', font=robotoSlab, color=(1, 0, 0, 1), padding=(0, 20))
             self.add_widget(label)
             
- 
+class TruckStartScreen(Screen):
+    def back(self):
+        screen_manager.transition.direction = 'up'
+        screen_manager.current = "start_screen"
         
-            
+class CreateTruckScreen(Screen):
+    def back(self):
+        screen_manager.transition.direction = 'right'
+        screen_manager.current = "truck_start_screen"
+
+    def createAccount(self):
+        emailValue = str(self.ids['email'].text)
+        truckNameValue = str(self.ids['truck_name'].text)
+        foodTypeValue = str(self.ids['food_type'].text)
+        passwordValue = str(self.ids['password'].text)
+
+        if(re.fullmatch(regex, emailValue)):
+            msg = MIMEMultipart('alternative')
+            msg['Subject'] = 'FoodTruckFinderMI Account'
+            msg['From'] = senderEmail
+            msg['To'] = emailValue
+                
+
+            emailbody = MIMEText(truckMSG, 'plain')
+            msg.attach(emailbody)
+            cursor.execute(f"INSERT INTO trucks (email, truckname, foodtype, isopen) VALUES ('{emailValue}', '{truckNameValue}', '{foodTypeValue}', {False})")
+            cnx.commit()
+            context = ssl.create_default_context()
+
+            try:
+                with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                    server.login(senderEmail, emailPassword)
+                    server.sendmail(senderEmail, emailValue, htmlMSG)
+                
+                
+            except:
+
+                screen_manager.transition.direction = 'left'
+                screen_manager.current = 'user_screen'
+                
+            else:
+                label = Label(text='Invalid Email Address', font=robotoSlab, color=(1, 0, 0, 1), padding=(0, 20))
+                self.add_widget(label)
 
 
-    
+
+
+
+
+class TruckLoginScreen(Screen):
+    def login(self):
+        emailValue = self.ids['email'].text
+        passwordValue = self.ids['password'].text
+        truckNameValue = self.ids["truck_name"].text
+
+        
+        cursor.execute(f"SELECT email from users WHERE email='{emailValue}' AND truckname = '{truckNameValue}' password = '{passwordValue}';")
+
+        
+        if not cursor.fetchone():  # An empty result evaluates to False.
+            print("Login failed")
+        else:
+            screen_manager.transition.direction = 'left'
+            screen_manager.current = 'user_screen'
+
+
+
+    def back(self):
+        screen_manager.transition.direction = 'right'
+        screen_manager.current = "truck_start_screen"
+
+
+
+class TruckPageScreen(Screen):
+    def openPage(self, truckName, foodType, IsOpen):
+        screen_manager.transition.direction = 'left'
+        screen_manager.current = 'truck_page_screen'
+
+
+
 
 class UserScreen(Screen):
     def load_Trucks(self):
-        box = Button()
-        buttonText = self.ids['LayoutForTrucks'].add_widget(box)
-        
 
+        
+        self.ids['LayoutForTrucks'].clear_widgets()
+
+        foodtypeOffset = 0
+        isopenOffset = 0
+
+        cursor.execute("SELECT truckname FROM trucks")
+        trucknames = cursor.fetchall()
+
+        cursor.execute("SELECT foodtype FROM trucks")
+        truckfoodtypes = cursor.fetchall()
+
+        cursor.execute("SELECT isopen FROM trucks")
+        truckisopen = cursor.fetchall()
+
+        for x in trucknames:
+            nameLabel = Label(text='Truck Name: ', font_name=robotoSlab)
+            truckNameLabel = Label(text=str(x[0]), font_name=robotoSlab)
+            foodLabel = Label(text="Food Type: ", font_name=robotoSlab)
+            foodTypeLabel = Label(text=str(truckfoodtypes[foodtypeOffset][0]), font_name=robotoSlab)
+            openLabel = Label(text="Currently Open: ", font_name=robotoSlab)
+            if truckisopen[isopenOffset][0] == 0:
+                isOpenLabel = Label(text="Closed", font_name=robotoSlab)
+            elif truckisopen[isopenOffset][0] == 0:
+                isOpenLabel = Label(text="Open", font_name=robotoSlab)
+            box = BoxLayout(orientation='vertical')
+            layout = GridLayout(cols = 2, padding=(0, 0, 0, 20))
+            
+            openPageButton = Button(text='Open Page', size_hint_y=.1)
+            openPageButton.bind(on_press=truckPageScreen.openPage(self, truckNameLabel.text, foodTypeLabel.text, isOpenLabel.text))
+            
+
+            layout.add_widget(nameLabel)
+            layout.add_widget(truckNameLabel)
+            layout.add_widget(foodLabel)
+            layout.add_widget(foodTypeLabel)
+            layout.add_widget(openLabel)
+            layout.add_widget(isOpenLabel)
+            box.add_widget(layout)
+            box.add_widget(openPageButton)
+            
+
+
+            buttonText = self.ids['LayoutForTrucks'].add_widget(box)
+
+            foodtypeOffset += 1
+            isopenOffset += 1
+    
+    
+        
+class TruckScreen(Screen):
+    pass
+
+
+
+class TruckPageScreen(Screen):
+    pass
 
 screen_manager.add_widget(StartScreen(name ="start_screen"))
 screen_manager.add_widget(LoginScreen(name ="login_screen"))
 screen_manager.add_widget(CreateAccountScreen(name="create_account_screen"))
+screen_manager.add_widget(TruckStartScreen(name="truck_start_screen"))
+screen_manager.add_widget(CreateTruckScreen(name='create_truck_screen'))
+screen_manager.add_widget(TruckLoginScreen(name='truck_login_screen'))
 screen_manager.add_widget(UserScreen(name ="user_screen"))
-
+screen_manager.add_widget(TruckPageScreen(name='truck_page_screen'))
 
 
 
